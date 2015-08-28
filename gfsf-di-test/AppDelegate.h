ee038c9c3666c3d33bf42b6e7ce3145c2a1389e9
Developer <tech@elo7.com>
2015-08-28 15:51:35 -0300

    Initial Commit

---
+++ gfsf-di-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gfsf-di-test
+//
+//  Created by Developer on 8/28/15.
+//  Copyright (c) 2015 Elo7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gfsf-di-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gfsf-di-test
+//
+//  Created by Developer on 8/28/15.
+//  Copyright (c) 2015 Elo7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
